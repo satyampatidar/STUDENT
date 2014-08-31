dacce2164a88cd4a19cbea4f7a40857a4666210d
SATYAM PATIDAR <satyam.patidar@gmail.com>
2014-08-31 07:00:08 +0530

    Update CourseSubjects.java

--- src/com/vedisoft/sis/CourseSubjects.java
+++ src/com/vedisoft/sis/CourseSubjects.java
@@ -1,32 +1,9 @@
-/**
- *
-/**
- * @author Rohit Ahuja
- * rohitahuja@email.com
- * 9893075987
- */
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * CourseSubjects.java
- *
- * Created on Apr 18, 2009, 5:43:42 PM
- */
 package com.vedisoft.sis;
 
 import java.util.Vector;
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Windows XP
- */
 public class CourseSubjects extends javax.swing.JPanel {
 
     /** Creates new form CourseSubjects */
@@ -147,14 +124,3 @@ public class CourseSubjects extends javax.swing.JPanel {
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
-/**
-* Vedisoft : Java - Module 1 "Desktop Technologies"
-*
-*
-*   Vedisoft Software & Education Services Pvt. Ltd.
-*   Plot No. 275, Zone II, M.P. Nagar,
-*   Bhopal.
-*   Phone : 0755 - 4076207, 4076208
-*   Email : contact@vedisoft.com
-*   Web : www.vedisoft.com
-*/
