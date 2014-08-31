b43ccf737506974579f7049f9d86979fc052712a
SATYAM PATIDAR <satyam.patidar@gmail.com>
2014-08-31 07:03:49 +0530

    Update MainFrame.java

--- src/com/vedisoft/sis/MainFrame.java
+++ src/com/vedisoft/sis/MainFrame.java
@@ -1,12 +1,3 @@
-/**
- *
-/**
- * @author Rohit Ahuja
- * rohitahuja@email.com
- * 9893075987
- */
-
-
 package com.vedisoft.sis;
 
 import javax.swing.*;
@@ -501,14 +492,3 @@ UIManager.LookAndFeelInfo looks[];
     private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
 }
-/**
-* Vedisoft : Java - Module 1 "Desktop Technologies"
-*
-*
-*   Vedisoft Software & Education Services Pvt. Ltd.
-*   Plot No. 275, Zone II, M.P. Nagar,
-*   Bhopal.
-*   Phone : 0755 - 4076207, 4076208
-*   Email : contact@vedisoft.com
-*   Web : www.vedisoft.com
-*/
