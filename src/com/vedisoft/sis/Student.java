c368b22707d1c167edb50118f6dfcd5f0d7e2b71
SATYAM PATIDAR <satyam.patidar@gmail.com>
2014-08-31 07:09:36 +0530

    Update Student.java

--- src/com/vedisoft/sis/Student.java
+++ src/com/vedisoft/sis/Student.java
@@ -1,12 +1,3 @@
-/**
- *
-/**
- * @author Rohit Ahuja
- * rohitahuja@email.com
- * 9893075987
- */
-//Table Creation
-
 /*
 Create Table Student(
 studentid int(7),
@@ -413,14 +404,3 @@ public class Student {
         System.out.println(ss.getRecordsOne(5, 3));
     }
 }
-/**
- * Vedisoft : Java - Module 1 "Desktop Technologies"
- *
- *
- *   Vedisoft Software & Education Services Pvt. Ltd.
- *   Plot No. 275, Zone II, M.P. Nagar,
- *   Bhopal.
- *   Phone : 0755 - 4076207, 4076208
- *   Email : contact@vedisoft.com
- *   Web : www.vedisoft.com
- */
