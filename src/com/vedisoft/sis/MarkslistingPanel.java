ed94e57ffc9b866365c1a4bf75a65386988eb47c
SATYAM PATIDAR <satyam.patidar@gmail.com>
2014-08-31 07:06:56 +0530

    Update MarkslistingPanel.java

--- src/com/vedisoft/sis/MarkslistingPanel.java
+++ src/com/vedisoft/sis/MarkslistingPanel.java
@@ -1,32 +1,9 @@
-/**
- *
-/**
- * @author Rohit Ahuja
- * rohitahuja@email.com
- * 9893075987
- */
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * MarkslistingPanel.java
- *
- * Created on Apr 25, 2009, 6:57:07 PM
- */
 package com.vedisoft.sis;
 
 import java.util.Vector;
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Administrator
- */
 public class MarkslistingPanel extends javax.swing.JPanel {
 
     /** Creates new form MarkslistingPanel */
@@ -155,14 +132,3 @@ public class MarkslistingPanel extends javax.swing.JPanel {
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
-/**
-* Vedisoft : Java - Module 1 "Desktop Technologies"
-*
-*
-*   Vedisoft Software & Education Services Pvt. Ltd.
-*   Plot No. 275, Zone II, M.P. Nagar,
-*   Bhopal.
-*   Phone : 0755 - 4076207, 4076208
-*   Email : contact@vedisoft.com
-*   Web : www.vedisoft.com
-*/
